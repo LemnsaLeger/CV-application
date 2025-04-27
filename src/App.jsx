c9134693e801af6f7d5ead37666d93e4a439f4a4
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-04-27 11:37:07 -0700

    Update Page With User Data And Handle Submit/Edit.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,79 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-import GeneralInfo from './components/General_Info.jsx';
-import Education from './components/Education.jsx';
-import Experience from './components/Experience.jsx';
+import GeneralInfo from "./components/General_Info.jsx";
+import Education from "./components/Education.jsx";
+import Experience from "./components/Experience.jsx";
 
 function App() {
+  const [GeneralInfos, setGeneralInfo] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+  const [Educations, setEducation] = useState({
+    schoolName: "",
+    title: "",
+    date: "",
+  });
+  const [Experiences, setExperiences] = useState({
+    companyRole: "",
+    companyName: "",
+    responsibilities: [],
+    dates: [],
+  });
+
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function handleSubmit() {
+    setIsSubmitted(!isSubmitted);
+  }
   return (
     <>
-      <GeneralInfo />
-      <Education />
-      <Experience />
+      {!isSubmitted ? (
+        <>
+          <GeneralInfo data={GeneralInfos} setData={setGeneralInfo} />
+          <Education data={Educations} setData={setEducation} />
+          <Experience data={Experiences} setData={setExperiences} />
+          <button onClick={handleSubmit}>
+            Submit All
+          </button>
+        </>
+      ) : (
+        <>
+          <h1>CV / Resume</h1>
+          <h2>General Information</h2>
+          <p>Name: {GeneralInfos.name}</p>
+          <p>Email: {GeneralInfos.email}</p>
+          <p>Phone: {GeneralInfos.phone}</p>
+
+          <h2>Education</h2>
+          <p>School: {Educations.schoolName}</p>
+          <p>Title: {Educations.title}</p>
+          <p>Date: {Educations.date}</p>
+
+          <h2>Experience</h2>
+          <p>Company Name: {Experiences.companyName}</p>
+          <p>Role: {Experiences.companyRole}</p>
+          <p>Responsibility(s)</p>
+          <ul>
+            {Experiences.responsibilities.map((resp) => (
+              <li key={resp}>{resp}</li>
+            ))}
+          </ul>
+
+          <p>Dates: </p>
+          <ul>
+            {Experiences.dates.map((date) => (
+              <li key={date}>{date}</li>
+            ))}
+          </ul>
+
+          <button onClick={handleSubmit}>Edit Cv Info</button>
+        </>
+      )}
     </>
   );
 }
 
-export default App
+export default App;
